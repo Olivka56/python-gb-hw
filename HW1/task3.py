3fa0eca6ac425687437f3aaf627d6cda3805cf1d
Vera <olivka130599@gmail.com>
2022-12-19 20:30:52 +0600

    HW1 task3

---
+++ HW1/task3.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+# в которой находится эта точка (или на какой оси она находится).
+
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+if x == 0 or y == 0:
+    print('Enter values not equal 0')
+else:
+    if x > 0 and y > 0:
+        print('1st quarter')
+    elif x < 0 and y > 0:
+        print('2nd quarter')
+    elif x < 0 and y < 0:
+        print('3rd quarter')
+    else:
+        print('4th quarter')
