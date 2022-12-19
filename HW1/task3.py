74509d9b413fd0a8f3efa368bdb3f5026a6bfc4b
Vera <olivka130599@gmail.com>
2022-12-19 21:01:33 +0600

    HW1 task3 int -> float

--- HW1/task3.py
+++ HW1/task3.py
@@ -1,8 +1,8 @@
 # Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
 # в которой находится эта точка (или на какой оси она находится).
 
-x = int(input('Enter x: '))
-y = int(input('Enter y: '))
+x = float(input('Enter x: '))
+y = float(input('Enter y: '))
 if x == 0 or y == 0:
     print('Enter values not equal 0')
 else:
